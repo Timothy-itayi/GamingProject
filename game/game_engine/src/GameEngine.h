6a305363344d332b1288c4ea43da898f7b366fc7
Timothy <timmytam10@gmail.com>
2025-02-08 15:07:27 +1100

    create a Scene Class and scene_composition object to for the Scene Class

---
+++ game/game_engine/src/GameEngine.h
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <vector>
+
+//Create a Scene Class for the game. 
+// This class will house how the Scenes will look like
+
+ class Scene {
+
+//We can start defining the object of our class with
+// name and description. This will be the bare bones of the project
+    public struct Scene_Composition{ 
+
+        std::string name;
+        std::string description
+       
+    }
+    // a method/function that we will define later
+    // choices will be used to handle what happens after a scene 
+     choices()
+ } 
+
+
+ int main () {
+
+ }
