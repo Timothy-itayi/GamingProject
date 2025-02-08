8f427870194666c2089e4794a8c357ba961b94d9
Timothy <timmytam10@gmail.com>
2025-02-08 19:47:42 +1100

    setup json format for scenes and choices

--- game/game_engine/src/GameEngine.h
+++ game/game_engine/src/GameEngine.h
@@ -1,4 +1,6 @@
+#pragma once
 #include <iostream> 
+#include "../external/json/json.hpp"
 #include <vector>
 
 //Create a Scene Class for the game. 
@@ -9,17 +11,40 @@
 //We can start defining the object of our class with
 // name and description. This will be the bare bones of the project
     public struct Scene_Composition{ 
-        std::int ID;
-        std::string name;
-        std::string description
+        std::int ID;           
+        std::string name;       
+        std::string description   
+       
        
     }
     // a method/function that we will define later
     // choices will be used to handle what happens after a scene 
-     choices()
+     display_choices();
+     process_choice();
  } 
 
 
- int main () {
+// Create a choice Struct 
 
- }
+struct Choice {
+    std::int ID;
+    std::char text;
+    std::int next_scene_id;
+
+}
+
+// Define a function to create a new choice
+Choice create_choice(int ID, const char text , int next_scene_id)
+
+// Define a function to process player choice
+int process_choice(Choice choices, int num_choices);
+
+// Define a function to display available choices
+void display_choices(Choice choices, int num_choices);
+
+
+
+
+
+
+#endif
